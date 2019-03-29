111c12895be2179ab544bd475b6d422a5fe82708
Michael Wuergler <wuergler@gmail.com>
2019-03-29 06:06:01 +0100

    werdino-daily now posts directly to Slack via webhook

--- index.js
+++ index.js
@@ -1,78 +1,116 @@
-'use strict';
-const werdino = require('werdino');
-const translate = require('google-translate-api');
-const condense = require('condense-whitespace');
-const titleize = require('titleize');
+'use strict'
+const werdino = require('werdino')
+const translate = require('google-translate-api')
+const condense = require('condense-whitespace')
+const fetch = require('fetch-everywhere')
 const zp = require('simple-zeropad');
-
-const today = new Date();
+const messageBuilder = require('./messageBuilder')
 
 // A key in the form of 2018-01-17
-const todaysItem = `${today.getFullYear()}-${zp(today.getMonth() + 1)}-${zp(today.getDate())}`;
+const today = new Date()
+const todaysItem = `${today.getFullYear()}-${zp(today.getMonth() + 1)}-${zp(
+  today.getDate()
+)}`
 
-function objectify(text) {
-	const parts = text.split('---\n');
+function objectify (text) {
+  const parts = text.split('---\n')
 
-	const meals = [];
+  const meals = []
 
-	parts.forEach(section => {
-		const obj = {};
+  parts.forEach(section => {
+    console.log({ section })
+    const obj = {}
 
-		section.split('\n').forEach(s => {
-			const title = '[TITLE] ';
-			const price = '[MEAL PRICES] ';
-			const descr = '[MEAL DESCRIPTION] ';
-			const mealTitle = '[MEAL TITLE] ';
+    section.split('\n').forEach(s => {
+      const title = '[TITLE] '
+      const price = '[MEAL PRICES] '
+      const descr = '[MEAL DESCRIPTION] '
+      const mealTitle = '[MEAL TITLE] '
 
-			let descText = '';
+      if (s.indexOf(title) === 0) {
+        obj.title = s.replace(title, '')
+      } else if (s.indexOf(mealTitle) === 0) {
+        obj.mealTitle = s.replace(mealTitle, '')
+      } else if (s.indexOf(descr) === 0) {
+        obj.description = s.replace(descr, '')
+      } else if (s.indexOf(price) === 0) {
+        obj.price = s.replace(price, '')
+      }
+    })
 
-			if (s.indexOf(title) === 0) {
-				obj.title = titleize(s.replace(title, ''));
-			} else if (s.indexOf(mealTitle) === 0) {
-				obj.mealTitle = titleize(s.replace(mealTitle, ''));
-			} else if (s.indexOf(descr) === 0) {
-				descText = s.replace(descr, '');
-
-				if (descText.trim().length > 0) {
-					obj.description = descText;
-				}
-			} else if (s.indexOf(price) === 0) {
-				obj.price = s.replace(price, '');
-			}
-		});
-
-		if (obj.title && obj.title !== '') {
-			meals.push(obj);
-		}
-	});
-	return meals;
+    if (obj.title && obj.title !== '') {
+      meals.push(obj)
+    }
+  })
+  return meals
 }
 
-module.exports = () => {
-	let german = '';
+const run = () => {
+  let german = ''
 
-	return new Promise(resolve => {
-		werdino().then(data => {
-			data.forEach(item => {
-				const title = condense(item.title);
+  return new Promise(resolve => {
+    werdino().then(data => {
+      data.forEach(item => {
+        // Don't break the JSON formatting when a single quote is present
+        const title = condense(item.title)
 
-				german += `[TITLE] ${title}\n`;
+        german += `[TITLE] ${title}\n`
 
-				const mealTitle = condense(item.meals[todaysItem].title);
-				const mealDescription = condense(item.meals[todaysItem].description);
+        const mealTitle = condense(item.meals[todaysItem].title)
+        const mealDescription = condense(item.meals[todaysItem].description)
 
-				german += `[MEAL TITLE] ${mealTitle}\n`;
-				german += `[MEAL DESCRIPTION] ${mealDescription}\n`;
-				german += `[MEAL PRICES] ${item.meals[todaysItem].prices.map(s => condense(s)).join(' | ')}\n`;
-				german += '---\n';
-			});
+        german += `[MEAL TITLE] ${mealTitle}\n`
 
-			translate(german).then(translated => {
-				resolve({
-					de: objectify(german),
-					en: objectify(translated.text)
-				});
-			});
-		});
-	});
-};
+        if (mealDescription) {
+          german += `[MEAL DESCRIPTION] ${mealDescription}\n`
+        }
+
+        german += `[MEAL PRICES] ${item.meals[todaysItem].prices
+          .map(s => condense(s))
+          .join(' | ')}\n`
+        german += '---\n'
+      })
+
+      translate(german).then(translated => {
+        const germanObject = objectify(german)
+        const englishObject = objectify(translated.text)
+
+        // Merga the english translations with the German to make the block building easier
+        englishObject.forEach((obj, index) => {
+          if (obj.title) {
+            germanObject[index].titleEn = obj.title
+          }
+          if (obj.mealTitle) {
+            germanObject[index].mealTitleEn = obj.mealTitle
+          }
+          if (obj.description) {
+            germanObject[index].descriptionEn = obj.description
+          }
+        })
+
+        resolve(germanObject)
+      })
+    })
+  })
+}
+
+run().then(data => {
+  const blocks = messageBuilder(data)
+
+  fetch(
+    'https://hooks.slack.com/services/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',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ blocks })
+    }
+  )
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => console.error('Error:', error))
+})
+
+module.exports = run
--- readme.md
+++ readme.md
@@ -1,38 +1,9 @@
-# werdino-daily [![Build Status](https://travis-ci.org/radiovisual/werdino-daily.svg?branch=master)](https://travis-ci.org/radiovisual/werdino-daily) [![Coverage Status](https://coveralls.io/repos/github/radiovisual/werdino-daily/badge.svg?branch=master)](https://coveralls.io/github/radiovisual/werdino-daily?branch=master)
+# werdino-daily
 
-> :pizza: :hamburger: The Werdino daily menu in English and German
-
-
-## Install
-
-```
-$ npm install werdino-daily
-```
-
-## Usage
-
-```js
-const werdino = require('werdino-daily');
-
-werdino().then(data => {
-    const { de, en } = data;
-}
-```
-
-## CLI Usage
-
-Install globally with `$ npm install -g werdino-daily` to get the cli version
-
-```
-$ werdino-daily
-```
+> :pizza: :hamburger: The Werdino daily menu in English and German, delivered straight to Slack
 
 ![](media/screenshot.png)
 
 ```
-$ werdino-daily md
+$ node index.js
 ```
-
-Output Markdown to paste the output into Slack or elsewhere.
-
-![image](https://user-images.githubusercontent.com/5614571/35171310-a98cce60-fd63-11e7-92c1-9e3ff3d8138c.png)
